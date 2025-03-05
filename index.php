aa10c80ca92c0a658b4d515cfd6254de56ce36e5
KANOUK1136 <canou_f@yahoo.fr>
2025-03-05 16:24:33 +0100

    modif list et br

--- index.php
+++ index.php
@@ -10,12 +10,12 @@
     <style>
     </style>
     <?php include 'navbar.php'; ?>
+    <br>
     <?php include 'banniere.php'; ?>
 </head>
   
   <body>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/index.css" />  
     <?php include 'accueil.php'; ?>
-    </body>
+  </body>
 </html>
--- list.php
+++ list.php
@@ -5,15 +5,17 @@
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
-    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">-->
-    <link rel="stylesheet" type="text/css" href="css/index.css" />
+    <link rel="stylesheet" type="text/css" href="css/list.css" />
     <style>
     </style>
     <?php include 'navbar.php'; ?>
+    <br>
     <?php include 'banniere.php'; ?>
 </head>
-  
-  <body>
+<br>
+<br>
+<body>
+    <link rel="stylesheet" type="text/css" href="css/list.css" />
     <div class="container text-center">
             <table class="table table-bordered">
                 <thead class="table-dark">
@@ -27,44 +29,248 @@
                     <tr>
                         <td>1</td>
                         <td>
-                            <strong>"Bust Through the Heavens with Your Drill!"</strong><br>
-                            <small>Transliteration: "Omae no doriru de ten o tsuke!"</small><br>
-                            <small>(<em>Japanese: お前のドリルで天を突け!</em>)</small>
+                            <strong>"Transperce les cieux avec ta vrille !"</strong><br>
+                            <small>Translittération : "Omae no doriru de ten o tsuke!"</small><br>
+                            <small>(<em>Japonais : お前のドリルで天を突け!</em>)</small>
                         </td>
                         <td>Masahiko Ōtsuka</td>
                     </tr>
                     <tr>
                         <td>2</td>
                         <td>
-                            <strong>"I Said I'm Gonna Pilot That Thing!!"</strong><br>
-                            <small>Transliteration: "Ore ga noru tte itten da!!"</small><br>
-                            <small>(<em>Japanese: 俺が乗るって言ってんだ!!</em>)</small>
+                            <strong>"Celui-là, c'est moi qui vais le piloter, je te dis !"</strong><br>
+                            <small>Translittération : "Ore ga noru tte itten da!!"</small><br>
+                            <small>(<em>Japonais : 俺が乗るって言ってんだ!!</em>)</small>
                         </td>
                         <td>Shouji Saeki</td>
                     </tr>
                     <tr>
-                      <td>2</td>
+                      <td>3</td>
                       <td>
-                          <strong>"I Said I'm Gonna Pilot That Thing!!"</strong><br>
-                          <small>Transliteration: "Ore ga noru tte itten da!!"</small><br>
-                          <small>(<em>Japanese: 俺が乗るって言ってんだ!!</em>)</small>
+                          <strong>"Arrête de te la péter avec tes deux têtes !"</strong><br>
+                          <small>Translittération : "Kao ga futatsu tā namaiki na!!"</small><br>
+                          <small>(<em>Japonais : 顔が2つたぁナマイキな!!</em>)</small>
                       </td>
-                      <td>Shouji Saeki</td>
+                      <td>Son Seung-heui</td>
                   </tr>
                   <tr>
-                    <td>2</td>
+                    <td>4</td>
                     <td>
-                        <strong>"I Said I'm Gonna Pilot That Thing!!"</strong><br>
-                        <small>Transliteration: "Ore ga noru tte itten da!!"</small><br>
-                        <small>(<em>Japanese: 俺が乗るって言ってんだ!!</em>)</small>
+                        <strong>"C'est pas le nombre de têtes qui compte !"</strong><br>
+                        <small>Translittération : "Kao ga ōkerya Erai no ka?"</small><br>
+                        <small>(<em>Japonais : 顔が多けりゃ偉いのか?</em>)</small>
+                    </td>
+                    <td>Osamu Kobayashi</td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>
+                        <strong>"Sérieux, je pige que dalle à tout ça !"</strong><br>
+                        <small>Translittération : "Ore ni wa sappari wakaranē!"</small><br>
+                        <small>(<em>Japonais : 俺にはさっぱりわからねぇ!</em>)</small>
+                    </td>
+                    <td>Hiroaki Tomita</td>
+                </tr>
+                <tr>
+                    <td>6</td>
+                    <td>
+                        <strong>"Dans la vie, y a des occases qu'un mec peut pas rater !"</strong><br>
+                        <small>Translittération : "Temēra zen'in yuatari shiyagare!!"</small><br>
+                        <small>(<em>Japonais : てめえら全員湯あたりしやがれ!!</em>)</small>
+                    </td>
+                    <td>Shin Itagaki</td>
+                </tr>
+                <tr>
+                    <td>7</td>
+                    <td>
+                        <strong>"C'est toi qui vas te charger du boulot !"</strong><br>
+                        <small>Translittération : "Sore wa omae ga yarun da yo!"</small><br>
+                        <small>(<em>Japonais : それはお前がやるんだよ!</em>)</small>
+                    </td>
+                    <td>Katsuichi Nakayama</td>
+                </tr>
+                <tr>
+                    <td>8</td>
+                    <td>
+                        <strong>"Adieu, mon Frère..."</strong><br>
+                        <small>Translittération : "Abayo, dachikō"</small><br>
+                        <small>(<em>Japonais : あばよ、ダチ公</em>)</small>
+                    </td>
+                    <td>Masahiko Ōtsuka</td>
+                </tr>
+                <tr>
+                    <td>9</td>
+                    <td>
+                        <strong>"Un humain ? Qu'est-ce que c'est exactement ?"</strong><br>
+                        <small>Translittération : "Hito tte ittai nan desu ka?"</small><br>
+                        <small>(<em>Japonais : ヒトっていったい何ですか?</em>)</small>
+                    </td>
+                    <td>Tarō Iwasaki</td>
+                </tr>
+                <tr>
+                    <td>10</td>
+                    <td>
+                        <strong>"Ton Frangin, quel genre de personne c'était ?"</strong><br>
+                        <small>Translittération : "Aniki tte ittai dare desu ka?"</small><br>
+                        <small>(<em>Japonais : アニキっていったい誰ですか?</em>)</small>
+                    </td>
+                    <td>Hiroshi Kurimoto</td>
+                </tr>
+                <tr>
+                    <td>11</td>
+                    <td>
+                        <strong>"Simon, est-ce que je peux ?"</strong><br>
+                        <small>Translittération : "Shimon, te o dokete"</small><br>
+                        <small>(<em>Japonais : シモン、手をどけて</em>)</small>
+                    </td>
+                    <td>Takayoshi Morimiya</td>
+                </tr>
+                <tr>
+                    <td>12</td>
+                    <td>
+                        <strong>"Mademoiselle Yoko, j'ai une faveur à vous demander"</strong><br>
+                        <small>Translittération : "Yōko-san, onegai ga arimasu"</small><br>
+                        <small>(<em>Japonais : ヨーコさん、お願いがあります</em>)</small>
+                    </td>
+                    <td>Hiroshi Kurimoto</td>
+                </tr>
+                <tr>
+                    <td>13</td>
+                    <td>
+                        <strong>"Bon appétit à tous !"</strong><br>
+                        <small>Translittération : "Mina-san, ta~n to meshiagare"</small><br>
+                        <small>(<em>Japonais : みなさん、た〜んと召し上がれ</em>)</small>
+                    </td>
+                    <td>Akitoshi Yokoyama</td>
+                </tr>
+                <tr>
+                    <td>14</td>
+                    <td>
+                        <strong>"Salutations à tous !"</strong><br>
+                        <small>Translittération : "Mina-san, gokigen'yō"</small><br>
+                        <small>(<em>Japonais : 皆さん、ごきげんよう</em>)</small>
+                    </td>
+                    <td>Katsuichi Nakayama</td>
+                </tr>
+                <tr>
+                    <td>15</td>
+                    <td>
+                        <strong>"Désormais, je regarderai vers le lendemain"</strong><br>
+                        <small>Translittération : "Watashi wa ashita e mukaimasu"</small><br>
+                        <small>(<em>Japonais : 私は明日へ向います</em>)</small>
+                    </td>
+                    <td>Ayumu Kotake</td>
+                </tr>
+                <tr>
+                    <td>16</td>
+                    <td>
+                        <strong>"Rétrospection"</strong><br>
+                        <small>Translittération : "Sōshūhen"</small><br>
+                        <small>(<em>Japonais : 総集片</em>)</small>
+                    </td>
+                    <td>Hiroyuki Yamaga</td>
+                </tr>
+                <tr>
+                    <td>17</td>
+                    <td>
+                        <strong>"Tu ne comprends vraiment rien à rien"</strong><br>
+                        <small>Translittération : "Anata wa nani mo wakatteinai"</small><br>
+                        <small>(<em>Japonais : あなたは何も分かっていない</em>)</small>
+                    </td>
+                    <td>Masahiko Ōtsuka</td>
+                </tr>
+                <tr>
+                    <td>18</td>
+                    <td>
+                        <strong>"Je veux tout savoir sur ce monde"</strong><br>
+                        <small>Translittération : "Kikasete morau zo kono sekai no nazo o"</small><br>
+                        <small>(<em>Japonais : 聞かせてもらうぞこの世界の謎を</em>)</small>
+                    </td>
+                    <td>Toshiya Shinohara</td>
+                </tr>
+                <tr>
+                    <td>19</td>
+                    <td>
+                        <strong>"Nous survivrons, quel que soit le moyen"</strong><br>
+                        <small>Translittération : "Ikinokorun da donna shudan o tsukatte mo"</small><br>
+                        <small>(<em>Japonais : 生き残るんだどんな手段を使っても</em>)</small>
+                    </td>
+                    <td>Yorifusa Yamaguchi</td>
+                </tr>
+                <tr>
+                    <td>20</td>
+                    <td>
+                        <strong>"Jusqu'où les Dieux nous mettront-ils à l'épreuve ?"</strong><br>
+                        <small>Translittération : "Kami wa doko made bokura o tamesu"</small><br>
+                        <small>(<em>Japonais : 神はどこまで僕らを試す</em>)</small>
+                    </td>
+                    <td>Yasuhiro Geshi</td>
+                </tr>
+                <tr>
+                    <td>21</td>
+                    <td>
+                        <strong>"Vous êtes de ceux qui doivent survivre"</strong><br>
+                        <small>Translittération : "Anata wa ikinokoru beki hito da"</small><br>
+                        <small>(<em>Japonais : あなたは生き残るべき人だ</em>)</small>
                     </td>
                     <td>Shouji Saeki</td>
                 </tr>
-                
-                </tbody>
-            </table>    
-  
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
-  </body>
+                <tr>
+                    <td>22</td>
+                    <td>
+                        <strong>"Ceci est mon ultime devoir !"</strong><br>
+                        <small>Translittération : "Sore ga boku no saigo no gimu da"</small><br>
+                        <small>(<em>Japonais : それが僕の最後の義務だ</em>)</small>
+                    </td>
+                    <td>Ikehiko Uda</td>
+                </tr>
+                <tr>
+                    <td>23</td>
+                    <td>
+                        <strong>"En route pour le combat final !"</strong><br>
+                        <small>Translittération : "Sayonara wa eien ni"</small><br>
+                        <small>(<em>Japonais : さよならは永遠に</em>)</small>
+                    </td>
+                    <td>Masahiko Ōtsuka</td>
+                </tr>
+                <tr>
+                    <td>24</td>
+                    <td>
+                        <strong>"Je n'oublierai jamais cette minute, cette seconde"</strong><br>
+                        <small>Translittération : "Wasureru mono ka kono ippun ichibyō o"</small><br>
+                        <small>(<em>Japonais : 忘れるものか この一分一秒を</em>)</small>
+                    </td>
+                    <td>Katsuichi Nakayama</td>
+                </tr>
+                <tr>
+                    <td>25</td>
+                    <td>
+                        <strong>"Que ta dernière volonté soit respectée!"</strong><br>
+                        <small>Translittération : "Omae no ishi wa uketotta!"</small><br>
+                        <small>(<em>Japonais : お前の遺志は受け取った!</em>)</small>
+                    </td>
+                    <td>Tōru Yoshida</td>
+                </tr>
+                <tr>
+                    <td>26</td>
+                    <td>
+                        <strong>"En avant camarades !"</strong><br>
+                        <small>Translittération : "Iku ze dachikō"</small><br>
+                        <small>(<em>Japonais : 行くぜ ダチ公</em>)</small>
+                    </td>
+                    <td>Masahiko Ōtsuka</td>
+                </tr>
+                <tr>
+                    <td>27</td>
+                    <td>
+                        <strong>"La piste des étoiles"</strong><br>
+                        <small>Translittération : "Ten no hikari wa subete hoshi"</small><br>
+                        <small>(<em>Japonais : 天の光はすべて星</em>)</small>
+                    </td>
+                    <td>Hiroyuki Imaishi</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
 </html>
