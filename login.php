ddbe10d123c0e3a6bc34b82af9a4906e05322eb9
KANOUK1136 <canou_f@yahoo.fr>
2025-03-12 15:39:25 +0100

    udpate formulaire

--- login.php
+++ login.php
@@ -1,34 +1,32 @@
-
-
-
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-            <meta charset="UTF-8">
-            <title>Page de connection</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            
-            <?php include 'navbar.php'; ?>
-            <link rel="stylesheet" type="text/css" href="css/login.css" />
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <title>Page de Connexion</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <?php include 'navbar.php'; ?>
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
 
-    <body>
-        
-        <form class="login-form" action="" method="post">
-            <h1>Connexion</h1>
-            <div class="form-input-material">
-                <input type="text" name="username" id="username" placeholder=" " autocomplete="off" class="form-control-material" required />
-                <label for="username">Identifiant</label>
-            </div>
+<body>
+    <form class="login-form" action="" method="post">
+        <h1>Connexion</h1>
 
-            <div class="form-input-material">
-                <input type="password" name="password" id="password" placeholder=" " autocomplete="off" class="form-control-material" required />
-                <label for="password">Password</label>
-            </div>
+        <div class="form-input-material">
+            <label for="username">Identifiant</label> 
+            <input type="text" name="username" id="username" autocomplete="off" class="form-control-material" required>
+        </div>
 
-            <button type="submit" class="btn btn-primary btn-ghost">Connexion</button>
+        <div class="form-input-material">
+            <label for="password">Mot de passe</label> 
+            <input type="password" name="password" id="password" autocomplete="off" class="form-control-material" required>
+        </div>
 
-            <?php
+        <button type="submit" class="btn btn-primary btn-ghost">Connexion</button>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $cookie_nom_id = "id";
             $cookie_identifiant = $_POST['username'];
             setcookie($cookie_nom_id, $cookie_identifiant, time() + (86400 * 30), "/");
@@ -36,16 +34,11 @@
             $cookie_mdp_id = "mdp";
             $cookie_password = $_POST['password'];
             setcookie($cookie_mdp_id, $cookie_password, time() + (86400 * 30), "/");
-            ?>
 
-            
-            <?php echo "Bienvenu : " . $_POST['username'] . "<br>Votre mdp est : " . $_POST['password']  ;?> 
-            <?php echo "<br><br>Cookie nom : " . $_COOKIE['id'];?>
-            <?php echo "<br>Cookie mdp: " . $_COOKIE['mdp'];?> 
-            
-            
-            
+            echo "<p>Bienvenue : " . htmlspecialchars($_POST['username']) . "</p>";
+        }
+        ?>
 
-        </form>
-        
-    </body>
+    </form>
+</body>
+</html>
